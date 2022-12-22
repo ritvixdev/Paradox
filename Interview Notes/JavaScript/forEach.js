7736bbe65d11529ae84c78a2fea179e912140f8b
Angry Boy <suvxdas@gmail.com>
2022-12-22 23:45:37 +0530

    forEach example

---
+++ Interview Notes/JavaScript/forEach.js
@@ -0,0 +1,7 @@
+//interview forEach exapmple
+
+const arr = ['a', 'b','c','d','e','f','g']
+
+arr.forEach((element, index, array) => {
+    console.log(`${element} in index ${index} of array ${array}`)
+})
