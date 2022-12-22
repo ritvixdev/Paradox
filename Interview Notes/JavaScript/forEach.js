97f18bbeb7337a44a46ca48ff2fa3412f5d0b041
Angry Boy <suvxdas@gmail.com>
2022-12-22 23:49:06 +0530

    forEach example

--- Interview Notes/JavaScript/forEach.js
+++ Interview Notes/JavaScript/forEach.js
@@ -5,3 +5,9 @@ const arr = ['a', 'b','c','d','e','f','g']
 arr.forEach((element, index, array) => {
     console.log(`${element} in index ${index} of array ${array}`)
 })
+
+// The forEach method passes a callback function for each element of an array together with the following parameters:
+
+// Current Value (required) - The value of the current array element
+// Index (optional) - The current element's index number
+// Array (optional) - The array object to which the current element belongs
