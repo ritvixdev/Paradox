7a8cd2f17705cd394d79401ea4b3c5d796b18b2d
Angry Boy <suvxdas@gmail.com>
2022-12-23 08:17:52 +0530

    Ways to iterrate Object in JavaScript

--- Interview Notes/JavaScript/ObjectIterate.js
+++ Interview Notes/JavaScript/ObjectIterate.js
@@ -1,35 +1,27 @@
 // iterrate through Javascript objects
 
 const population = {
-    male: 4,
-    female: 93,
-    others: 10
-  };
-  
-  let genders = Object.keys(population);
+  male: 4,
+  female: 93,
+  others: 10,
+};
 
-  genders.forEach((gender) => {
-    console.log(`There are ${population[gender]} ${gender}`);
-  })
+let genders = Object.keys(population);
 
+genders.forEach((gender) => {
+  console.log(`There are ${population[gender]} ${gender}`);
+});
 
 //====== sum of all population
 
-//   const population = {
-//     male: 4,
-//     female: 93,
-//     others: 10
-//   };
-  
-  let totalPopulation = 0;
+let totalPopulation = 0;
 //   let genders = Object.keys(population);
-  
-  genders.forEach((gender) => {
-    totalPopulation += population[gender];
-  });
-  
-  console.log(totalPopulation); // 107
 
+genders.forEach((gender) => {
+  totalPopulation += population[gender];
+});
+
+console.log(totalPopulation); // 107
 
 //====== Sum by object.values()
 
@@ -40,28 +32,41 @@ numbers.forEach((number) => {
   totalPopulation += number;
 });
 
-console.log(totalPopulation); 
+console.log(totalPopulation);
 
 //==== Use of Object.entries
 
-// const population = {
-//     male: 4,
-//     female: 93,
-//     others: 10
-//   };
-  
-  let populationArr = Object.entries(population);
-  
-  console.log(populationArr);
+let populationArr = Object.entries(population);
+
+console.log(populationArr);
 
 // return array
 
-for (array of populationArr){
-    console.log(array);
-  }
+for (array of populationArr) {
+  console.log(array);
+}
 
 // get the keys
 
-for ([key, value] of populationArr){
-    console.log(key);
-  } 
+for ([key, value] of populationArr) {
+  console.log(key);
+}
+
+//======== other Example=============
+
+var obj = { first: "John", last: "Doe" };
+
+for (const key of Object.keys(obj)) {
+  console.log(key, obj[key]);
+}
+// ECMAScript 8 adds Object.entries() which avoids having to look up each value in the original object:
+
+Object.entries(obj).forEach(([key, value]) => console.log(key, value));
+// You can combine for...of, destructuring, and Object.entries:
+
+for (const [key, value] of Object.entries(obj)) {
+  console.log(key, value);
+}
+// Both Object.keys() and Object.entries() iterate properties-
+// in the same order as a for...in loop but ignore the prototype chain.
+// Only the object's own enumerable properties are iterated.
