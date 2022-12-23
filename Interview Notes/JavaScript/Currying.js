d74fbbcba1c13a93e345f2f908d1e87abc7c4812
Angry Boy <suvxdas@gmail.com>
2022-12-23 11:41:18 +0530

    All about curring

--- Interview Notes/JavaScript/Currying.js
+++ Interview Notes/JavaScript/Currying.js
@@ -10,11 +10,32 @@ const addCurry = (a) => {
 
 console.log(addCurry(1)(2)(3));
 
-
 // by normal function
 
-const add = (a,b,c) => {
-    return a+b+c;
-}
+const add = (a, b, c) => {
+  return a + b + c;
+};
 
-console.log(add(1,2,3))
+console.log(add(1, 2, 3));
+
+// Advance Currying
+
+const curry = (fn) => {
+  return (curried = (...args) => {
+    if (fn.length !== args.length) {
+      return curried.bind(null, ...args);
+    }
+    return fn(...args);
+  });
+};
+const totalNum = (x, y, z) => {
+  return x + y + z;
+};
+const curriedTotal = curry(totalNum);
+console.log(curriedTotal(10)(20)(30));
+
+// Modern Currying with ES6
+
+const sendRequest = (greet) => (name) => (message) =>
+  console.log(`${greet} ${name}, ${message}`);
+sendRequest("Hello")("John")("Please can you add me to your Linkedin network?");
