a4b94881da5d0f0bdbfd9f8c75025cfdff37714b
Angry Boy <suvxdas@gmail.com>
2022-12-23 11:55:46 +0530

    Promises updated

--- Interview Notes/JavaScript/Promise.js
+++ Interview Notes/JavaScript/Promise.js
@@ -14,3 +14,31 @@ const myPromise = new Promise((resolve, reject) => {
 myPromise
 .then((msg) => console.log(msg))
 .catch((err) => console.log(err))
+
+
+// by Async/Await
+
+const myPromises = () => {
+  return new Promise((resolve, reject) => {
+  const condition = true
+  if(condition){
+    setTimeout(() => {
+      resolve("promise is resolved")
+    },1000)
+  }else{
+    reject("promise is rejected")
+  }
+})
+}
+
+const demoPromise = async () => {
+  try{
+    const msg = await myPromises();
+    console.log(msg)
+  }catch(err){
+    console.log(err)
+  }
+}
+
+demoPromise()
+
