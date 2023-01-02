3c8b6bec286cecf9e00b244e179417397ebfaeaf
Angry Boy <suvxdas@gmail.com>
2023-01-02 20:05:59 +0530

    Observable + Promise VS Observable

--- Coding Questions/JavaScript/Observable.js
+++ Coding Questions/JavaScript/Observable.js
@@ -45,18 +45,18 @@ observable.subscribe();
 // ===Promise Vs Observable===
 
 // Observables are lazy whereas promises are not
-// This is pretty self-explanatory: observables are lazy, that is we have to subscribe 
-// observables to get the results. In the case of promises, they execute immediately.
+// -This is pretty self-explanatory: observables are lazy, that is we have to subscribe 
+//  observables to get the results. In the case of promises, they execute immediately.
 
 // Observables handle multiple values unlike promises
-// Promises can only provide a single value whereas observables can give you multiple values.
+// -Promises can only provide a single value whereas observables can give you multiple values.
 
 // Observables are cancelable
-// You can cancel observables by unsubscribing it using the unsubscribe method 
-// whereas promises don’t have such a feature.
+// -You can cancel observables by unsubscribing it using the unsubscribe method 
+//  whereas promises don’t have such a feature.
 
 // Observables provide many operators
-// There are many operators like map, forEach, filter etc. Observables provide 
-// these whereas promises does not have any operators in their bucket.
+// -There are many operators like map, forEach, filter etc. Observables provide 
+//  these whereas promises does not have any operators in their bucket.
 
 
