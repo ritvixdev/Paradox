18a0c73834de9f0906566c0dab46a7aa16acd5a4
Angry Boy <suvxdas@gmail.com>
2023-01-03 09:14:53 +0530

    Async/Await way to fetch data

--- Coding Questions/JavaScript/AysncFecthAPI.js
+++ Coding Questions/JavaScript/AysncFecthAPI.js
@@ -1,16 +1,16 @@
 //Async/Await way to fetch data
 
-const url = "https://pokeapi.co/api/v2/pokemon?limit=10"
+const url = 'https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001';
 
 const fetchData = async () => {
-  try{
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data)
-    
-  }catch(err){
-    console.log(err)
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
   }
-}
+};
 
-fetchData()
+fetchData();
+
