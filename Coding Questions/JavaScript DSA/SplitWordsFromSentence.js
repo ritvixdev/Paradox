4141610da77bfd8f3e0838edc665a03b363464b7
Angry Boy <suvxdas@gmail.com>
2023-01-09 20:42:57 +0530

    Javascript programm to split words form sentence

---
+++ Coding Questions/JavaScript DSA/SplitWordsFromSentence.js
@@ -0,0 +1,20 @@
+// Javascript programm to split words form sentence
+
+let sentence = 'This is a sample sentence.';
+
+const splitIntoWords = (sentence) => {
+  let words = [];
+  let word = '';
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === ' ') {
+      words.push(word);
+      word = '';
+    } else {
+      word += sentence[i];
+    }
+  }
+  words.push(word);
+  return words;
+};
+
+console.log(splitIntoWords(sentence));
