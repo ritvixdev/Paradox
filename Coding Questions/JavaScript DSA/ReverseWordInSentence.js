93e6be41098a7de70d9b99ac48987d98cf51a5ce
Angry Boy <suvxdas@gmail.com>
2023-01-09 20:34:07 +0530

    Reverse words in sentense whole length more than 3

--- Coding Questions/JavaScript DSA/ReverseWordInSentence.js
+++ Coding Questions/JavaScript DSA/ReverseWordInSentence.js
@@ -23,3 +23,44 @@ const operation = (str) => {
 
 console.log(operation(str)); // [ 'ereht', 'is', 'a', 'man', 'in', 'the', 'dlrow' ]
 
+
+// without using build in method split
+
+str = 'there is a man in the world';
+
+const rev1 = (word) => {
+  let rev = '';
+  for (let i = 0; i <= word.length - 1; i++) {
+    rev = word[i] + rev;
+  }
+  return rev;
+};
+
+const splitWord = (str) => {
+  words = []
+  idx= 0
+  temp = ''
+  for(let i=0;i<=str.length-1;i++){
+    if(str[i] != ' '){
+      temp = temp+str[i]
+    }else{
+      words[idx++] = temp
+      temp =''
+    }
+  }
+  return words
+}
+
+const operations = (str) => {
+  let word = splitWord(str);
+  let res = [];
+  for (let i = 0; i <= word.length - 1; i++) {
+    if (word[i].length > 3) {
+      word[i] = rev1(word[i]);
+    }
+  }
+  return word;
+};
+
+console.log(operations(str));
+
