3ead4b47e50cfa90e4dbc02b19390be2ef001d60
Angry Boy <suvxdas@gmail.com>
2023-01-10 01:24:57 +0530

    Event Emittter

---
+++ Coding Questions/NodeJsHacker/OrderProcessing.js
@@ -0,0 +1,51 @@
+// Event Emittter Order Processing
+
+// By class: Not solved
+
+// Payload:
+
+[
+    {
+        'orderNumber': "0D2323",
+        "lineItems": [
+            {
+                "itemId":3,
+                "quantity" :4
+            },
+            {
+                "itemId":3,
+                "quantity" :4
+            }
+        ]
+    }
+]
+
+
+// JSON FILE: stock-list.json
+
+[
+    {
+        "id" : 0,
+        "stock" : 4
+    },
+    {
+        "id" : 1,
+        "stock" : 12
+    },
+    {
+        "id" : 2,
+        "stock" : 2
+    },
+    {
+        "id" : 3,
+        "stock" : 6
+    },
+    {
+        "id" : 4,
+        "stock" : 40
+    },
+    {
+        "id" : 5,
+        "stock" : 3
+    }
+]
