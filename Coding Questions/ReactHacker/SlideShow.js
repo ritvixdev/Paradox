0e9f46cba18674c4f6d7a54bb536352eeeda9122
Angry Boy <suvxdas@gmail.com>
2023-01-10 12:36:53 +0530

    sllideshow application in rect JS

---
+++ Coding Questions/ReactHacker/SlideShow.js
@@ -0,0 +1,58 @@
+// create a basic sllideshow application in rect JS
+function Slides({ slides }) {
+
+
+  const [activeSlideNo, setActiveSlideNo] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(slides[0]);
+  const [disablePrev, setDisablePrev] = useState(true);
+  const [disableNext, setDisableNext] = useState(true);
+  const [disableRestart, setDisableRestart] = useState(true);
+
+  const onCllickNext = () => {
+    let curSlideNo = activeSlideNo;
+    if (curSlideNo < slides.length - 1) {
+      curSlideNo++;
+      setActiveSlideNo(curSlideNo);
+      setCurrentSlide(slides[curSlideNo]);
+      setDisablePrev(false);
+      setDisableRestart(false);
+    }
+
+    if (curSlideNo == slides.length - 1) {
+      setdisableNext(true);
+    }
+  };
+
+  const onClickPrev = () => {
+    let curSlideNo = activeSlideNo;
+    if (curSlideNo > 0) {
+      curSlideNo--;
+      setActiveSlideNo(curSlideNo);
+      setCurrentSlide(slides[curSlideNo]);
+      setDisableNext(false);
+    }
+
+    if (curSlideNo == 0) {
+      setdisablePrev(true);
+      setDisableRestart(true);
+    }
+  };
+
+  const onCLickRestart = () => {
+    setActiveSlideNo(0);
+    setCurrentSlide(slides[0]);
+    setDisableNext(false);
+    setdisablePrev(true);
+    setDisableRestart(true);
+  };
+
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button disabled={disableRestart} onClick={() => onClickRestart()}>Restart</button>
+        <button disabled={disablePrev} onClick={() => onClickPrev()}>Prev</button>
+        <button disabled={disableNext} onClick={() => onClickNext()}>Next</button>
+      </div>
+    </div>
+  );
+}
