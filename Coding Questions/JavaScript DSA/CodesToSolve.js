802a33efc4a05f76d758d80255e63c84d6a7954d
Angry Boy <suvxdas@gmail.com>
2023-01-11 09:05:50 +0530

    s

---
+++ Coding Questions/JavaScript DSA/CodesToSolve.js
@@ -0,0 +1,56 @@
+// Code to predict output:
+
+(function () {
+  setTimeout(() => console.log(1), 2000);
+  console.log(2);
+  setTimeout(() => console.log(3), 0);
+  console.log(4);
+})(); // 2,4,3,1
+
+// second Code:
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, i * 1000);
+} // 3,3,3
+
+// third code:
+
+function checkAge(data) {
+  if (data === { age: 18 }) {
+    console.log("You are an adult!");
+  } else if (data == { age: 18 }) {
+    console.log("You are still an adult.");
+  } else {
+    console.log(`Hmm.. You don't have an age I guess`);
+  }
+}
+checkAge({ age: 18 }); // Hmm.. You don't have an age I guess
+
+
+// Explaination:
+
+//The output of this code will be false.
+// The reason for this is that the == operator compares the references 
+// of the two objects, rather than their values. Even though obj1 and obj2 
+// have the same properties with the same values, they are two separate objects 
+// in memory and therefore have different references.
+
+// In javascript, Objects and arrays are compared by reference, they are not primitive types. 
+// To compare two objects or arrays by their values, you would have to write a function 
+// to iterate through the properties and compare them. you can use JSON.stringify method 
+// and compare the result or you can use lodash library method like isEqual().
+
+
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2))
+
+// or
+
+const isEqual = require('lodash').isEqual;
+console.log(isEqual(obj1, obj2))
+
+// Both of the above will return true
+
+// Keep in mind that deep comparison like this could be slower than just checking 
+// for references, so it's important to use it only when it's necessary.
