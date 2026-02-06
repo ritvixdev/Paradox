b80a61566c220945670e3e74da0b553a57235d85
RitviX <70150943+ritvixdev@users.noreply.github.com>
2026-02-06 12:52:54 +0530

    Implement anagram check using two frequency maps
    Added a second method to check for anagrams using two frequency maps.

--- Coding Questions/JavaScript DSA/Anagram.js
+++ Coding Questions/JavaScript DSA/Anagram.js
@@ -55,6 +55,41 @@ var isAnagram = function (str1, str2) {
 console.log(isAnagram(str1,str2))
 
 
+// second method
+var isAnagramUsingTwoMaps = function (str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const freq1 = {};
+  const freq2 = {};
+
+  for (let i = 0; i < str1.length; i++) {
+    const ch = str1[i];
+    if (freq1[ch] === undefined) {
+      freq1[ch] = 1;
+    } else {
+      freq1[ch]++;
+    }
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    const ch = str2[i];
+    if (freq2[ch] === undefined) {
+      freq2[ch] = 1;
+    } else {
+      freq2[ch]++;
+    }
+  }
+
+  for (let key in freq1) {
+    if (freq1[key] !== freq2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+console.log(isAnagramUsingTwoMaps('listen', 'silent'));
 
 
 
